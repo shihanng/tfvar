37223d089f11d4b08204e3eedc1c7465b819ff22
Shi Han NG <shihanng@gmail.com>
2020-02-01 09:56:38 +0900

    tfvar: Move parse to collect and cover more test

--- pkg/tfvar/collect.go
+++ pkg/tfvar/collect.go
@@ -118,3 +118,21 @@ func (v unparsedVariableValueExpression) ParseVariableValue(_ configs.VariablePa
 
 	return val, nil
 }
+
+func ParseValues(from map[string]UnparsedVariableValue, vars []Variable) ([]Variable, error) {
+	for i, v := range vars {
+		unparsed, found := from[v.Name]
+		if !found {
+			continue
+		}
+
+		val, err := unparsed.ParseVariableValue(v.parsingMode)
+		if err != nil {
+			return nil, err
+		}
+
+		vars[i].Value = val
+	}
+
+	return vars, nil
+}
--- pkg/tfvar/collect_test.go
+++ pkg/tfvar/collect_test.go
@@ -4,6 +4,8 @@ import (
 	"os"
 	"testing"
 
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/terraform/configs"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/zclconf/go-cty/cty"
@@ -157,4 +159,92 @@ func TestCollectFromFile(t *testing.T) {
 			assert.Equal(t, tt.want, actual)
 		})
 	}
+}
+
+func TestParseValues(t *testing.T) {
+	type args struct {
+		from map[string]UnparsedVariableValue
+		vars []Variable
+	}
+
+	tests := []struct {
+		name      string
+		args      args
+		want      []Variable
+		assertion assert.ErrorAssertionFunc
+	}{
+		{
+			name: "normal",
+			args: args{
+				from: map[string]UnparsedVariableValue{
+					"a": unparsedVariableValueString{str: "val", name: "a"},
+				},
+				vars: []Variable{
+					{Name: "a", parsingMode: configs.VariableParseLiteral},
+					{Name: "b", parsingMode: configs.VariableParseLiteral},
+				},
+			},
+			want: []Variable{
+				{Name: "a", Value: cty.StringVal("val"), parsingMode: configs.VariableParseLiteral},
+				{Name: "b", parsingMode: configs.VariableParseLiteral},
+			},
+			assertion: assert.NoError,
+		},
+		{
+			name: "failed parsing mode",
+			args: args{
+				from: map[string]UnparsedVariableValue{
+					"a": unparsedVariableValueString{str: "val", name: "a"},
+				},
+				vars: []Variable{
+					{Name: "a", parsingMode: configs.VariableParseHCL},
+				},
+			},
+			want:      nil,
+			assertion: assert.Error,
+		},
+		{
+			name: "fail parsing expression",
+			args: args{
+				from: map[string]UnparsedVariableValue{
+					"a": unparsedVariableValueExpression{
+						expr: mockExpr{},
+					},
+				},
+				vars: []Variable{
+					{Name: "a", parsingMode: configs.VariableParseHCL},
+				},
+			},
+			want:      nil,
+			assertion: assert.Error,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := ParseValues(tt.args.from, tt.args.vars)
+			tt.assertion(t, err)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+type mockExpr struct{}
+
+func (e mockExpr) Value(_ *hcl.EvalContext) (cty.Value, hcl.Diagnostics) {
+	return cty.Value{}, hcl.Diagnostics{
+		&hcl.Diagnostic{Severity: hcl.DiagError},
+	}
+}
+
+func (e mockExpr) Variables() []hcl.Traversal {
+	return nil
+}
+
+func (e mockExpr) Range() hcl.Range {
+	return hcl.Range{}
+}
+
+func (e mockExpr) StartRange() hcl.Range {
+	return hcl.Range{}
 }
