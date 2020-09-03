068f4955f2585913d1823b8f390fb9a4118d00a9
Shi Han NG <shihanng@gmail.com>
2020-09-03 18:59:31 +0900

    Export map/object in single line

--- pkg/tfvar/testdata/defaults/main.tf
+++ pkg/tfvar/testdata/defaults/main.tf
@@ -20,3 +20,27 @@ output "instance_name" {
 output "availability_zone_names" {
   value = var.availability_zone_names
 }
+
+variable "aws_amis" {
+  default = {
+    "eu-west-1" = "ami-b1cf19c6"
+    "us-east-1" = "ami-de7ab6b6"
+    "us-west-1" = "ami-3f75767a"
+    "us-west-2" = "ami-21f78e11"
+  }
+}
+
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8301
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
